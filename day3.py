b55fbc20077f31aafaab9c88d71d096214b9af54
Asher Diamant <asher.diamant@gmail.com>
2020-12-03 16:20:27 -0500

    Solved 3b

--- day3.py
+++ day3.py
@@ -1,18 +1,29 @@
 import numpy as np
 from collections import Counter
 
-grid = np.loadtxt('data/day3.txt', comments=None, dtype=np.str).reshape(-1, 1)
-step = np.array([1, 3])
-slope = step[1]/step[0]
+input_grid = np.loadtxt('data/day3.txt', comments=None, dtype=np.str).reshape(-1, 1)
 
-traverse = slope * grid.shape[0]
-repeats = int(traverse/len(grid[0]))
-full_grid = np.tile(grid, (1, repeats+1))
-full_grid = np.array([''.join(row) for row in full_grid])
 
-route = [full_grid[0][0]]
-for i in range(1, full_grid.shape[0]):
-    next_element = i*step
-    route.append(full_grid[next_element[0]][next_element[1]])
+def count_trees(grid, step):
+    slope = step[1] / step[0]
 
-print(Counter(route))
+    traverse = slope * grid.shape[0]
+    repeats = int(traverse / len(grid[0]))
+    full_grid = np.tile(grid, (1, repeats + 1))
+    full_grid = np.array([''.join(row) for row in full_grid])
+
+    route = [full_grid[0][0]]
+    for i in range(1, int(full_grid.shape[0]/step[0])):
+        next_element = i * step
+        route.append(full_grid[next_element[0]][next_element[1]])
+
+    return Counter(route)['#']
+
+
+trees = []
+steps = np.array([[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]])
+for this_step in steps:
+    result = (this_step, count_trees(input_grid, this_step))
+    print(result)
+    trees.append(result[1])
+print(np.array(trees).prod())
