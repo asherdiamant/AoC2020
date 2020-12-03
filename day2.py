6754902bd66a23583b2ec12ed0612ea8e7224c76
Asher Diamant <asher.diamant@gmail.com>
2020-12-03 12:30:24 -0500

    Solved 2b

--- day2.py
+++ day2.py
@@ -9,8 +9,18 @@ def is_valid(pw_entry):
     return int(pm['minreps']) <= reps <= int(pm['maxreps'])
 
 
+def is_valid2(pw_entry):
+    pm = re.match(r'(?P<first>\d+)-(?P<second>\d+) (?P<testchar>.): (?P<pattern>\w+)', pw_entry.rstrip())
+    first_loc = int(pm['first']) - 1
+    second_loc = int(pm['second']) - 1
+    return(pm['testchar']==pm['pattern'][first_loc]) != (pm['testchar']==pm['pattern'][second_loc])
+
+
 with open('data/day2.txt', 'r') as f:
     password_db = f.readlines()
 
 valid_pws = [is_valid(x) for x in password_db]
 print(sum(valid_pws))
+
+valid_pws2 = [is_valid2(x) for x in password_db]
+print(sum(valid_pws2))
