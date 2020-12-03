dabe0b0ca82deeb71450eea607fe929b96143b8a
Asher Diamant <asher.diamant@gmail.com>
2020-12-03 11:52:51 -0500

    Solved 1b

--- day1.py
+++ day1.py
@@ -3,4 +3,8 @@ import numpy as np
 expense = np.loadtxt('data/day1a.txt', dtype=int)
 pairs = np.array(np.meshgrid(expense, expense)).T.reshape(-1, 2)
 entries = pairs[pairs.sum(axis=1)==2020][0]
-print(entries.prod())
+print(entries, entries.prod())
+
+trips = np.array(np.meshgrid(expense, expense, expense)).T.reshape(-1, 3)
+entries = trips[trips.sum(axis=1)==2020][0]
+print(entries, entries.prod())
