8bf5c7030f23cdc5682adcdb29b3eed93ca7bf93
Asher Diamant <asher.diamant@gmail.com>
2020-12-05 23:18:01 -0500

    Solved 5b

--- day5.py
+++ day5.py
@@ -17,7 +17,15 @@ with open('data/day5.txt', 'r') as f:
     for row in f.readlines():
         bp = BoardingPass(row)
         boarding_passes.append(bp)
-print(max(bp.id for bp in boarding_passes))
+max_id = max(bp.id for bp in boarding_passes)
+print('Highest seat ID is:', max_id )
 
+
+min_id = min(bp.id for bp in boarding_passes)
+print('Lowest seat ID is:', min_id)
+
+for i in range(min_id, max_id):
+    if i not in (bp.id for bp in boarding_passes):
+        print("Empty seat, possibly mine:", i)
 
 
