ca26c59a716149060f41997c2428a513980738ee
Asher Diamant <asher.diamant@gmail.com>
2020-12-06 23:02:35 -0500

    Solved 6b

--- day6.py
+++ day6.py
@@ -1,4 +1,6 @@
+import numpy as np
 from collections import defaultdict
+
 
 group_count = 0
 group_members = 0
@@ -9,12 +11,14 @@ with open('data/day6.txt') as f:
     for line in f.readlines()+['\n']:
         if line == '\n':
             group_count += 1
-            customs_forms.append((group_count, group_members, len(answers.keys()), answers))
+            customs_forms.append((group_count, group_members, answers))
             answers = defaultdict(int)
+            group_members = 0
         else:
-            print(line)
             group_members += 1
             for c in line.rstrip():
                 answers[c] += 1
-            print(answers)
-print('Total unique yes answers:', sum(form[2] for form in customs_forms))
+
+print('Total unique yes answers: ', sum(len(form[2]) for form in customs_forms))
+
+print('Total unanimous yes answers: ', sum(sum(np.array(list(form[2].values())) == form[1]) for form in customs_forms))
