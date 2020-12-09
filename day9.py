49d913d0de3df43af01df7b988a1b3ef8981ca1e
Asher Diamant <asher.diamant@gmail.com>
2020-12-09 11:35:20 -0500

    Solved 9b

--- day9.py
+++ day9.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 cyphertext = np.loadtxt('data/day9.txt', dtype=int)
-indices = range(25)]
+indices = range(25)
 pairs = np.array(np.meshgrid(indices, indices)).T.reshape(-1, 2)
 valid_pairs = [pair for pair in pairs if pair[0] != pair[1]]
 
@@ -19,4 +19,18 @@ for i in range(len(cyphertext)-25):
         first_invalid = curr_value
         print(f"{i+25}: {curr_value} not valid.")
         break
+
+
+for start in range(len(cyphertext)-1):
+    for length in range(2, len(cyphertext) - start):
+        total = np.sum(cyphertext[start:start+length])
+        if total > first_invalid:
+            break
+        if total == first_invalid:
+            print(f"Sequence summing up to {first_invalid}: \n", "start:", start, "length:", length, cyphertext[start:start+length])
+            smallest = np.min(cyphertext[start:start+length])
+            largest = np.max(cyphertext[start:start+length])
+            print(f"Found! {smallest} + {largest} = {smallest + largest}")
+            exit(0)
+print("Not found :(")
 
