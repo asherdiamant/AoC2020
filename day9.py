be5a7f674f83401f1ba39a2141be862a29b0d17a
Asher Diamant <asher.diamant@gmail.com>
2020-12-09 11:22:44 -0500

    Solved 9a - cleaned up

--- day9.py
+++ day9.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+cyphertext = np.loadtxt('data/day9.txt', dtype=int)
+indices = range(25)]
+pairs = np.array(np.meshgrid(indices, indices)).T.reshape(-1, 2)
+valid_pairs = [pair for pair in pairs if pair[0] != pair[1]]
+
 
 def calc_all_sums(array, pairs_list, shift):
     all_sums = []
@@ -7,18 +12,11 @@ def calc_all_sums(array, pairs_list, shift):
         all_sums.append(array[pair[0]+shift] + array[pair[1]+shift])
     return set(all_sums)
 
-
-cyphertext = np.loadtxt('data/day9.txt', dtype=int)
-indices = range(25)
-#    preamble = cyphertext[i-25:i]
-pairs = np.array(np.meshgrid(indices, indices)).T.reshape(-1, 2)
-#    all_sums = pairs.sum(axis=1)
-
-valid_pairs = [pair for pair in pairs if pair[0] != pair[1]]
-
 for i in range(len(cyphertext)-25):
     curr_value = cyphertext[i+25]
     valid_sums = calc_all_sums(cyphertext, valid_pairs, i)
     if curr_value not in valid_sums:
+        first_invalid = curr_value
         print(f"{i+25}: {curr_value} not valid.")
         break
+
